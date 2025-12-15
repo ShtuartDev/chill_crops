c0573549a9d3114a5d0568ae24545a02627aacde
ŠtuartDev <69080539+ShtuartDev@users.noreply.github.com>
2025-12-15 23:35:19 +0100

    Add files via upload

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -6,16 +6,16 @@ func _on_button_pressed() -> void:
 		Globals.cash+=3
 
 func _on_button_2_pressed() -> void:
-	if Globals.cash==3:
+	if Globals.cash>=3:
 		Globals.cash-=3
 		Globals.max_actions+=2
 
 func _on_button_3_pressed() -> void:
-	if Globals.cash==10:
+	if Globals.cash>=10:
 		Globals.cash-=10
 		Globals.blanky+=1
 
 func _on_button_4_pressed() -> void:
-	if Globals.cash==50:
+	if Globals.cash>=50:
 		Globals.cash-=50
 		Globals.day-=1
